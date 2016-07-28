3050cd9ce681d53faee0785c9eec4cc7d856e647
Wes Widner <kai5263499@gmail.com>
2016-07-28 07:25:33 -0400

    hexfiend 2.1.2:  New formula

---
+++ Casks/hexfiend.rb
@@ -0,0 +1,12 @@
+cask 'hexfiend' do
+  version '2.1.2'
+  sha1 'b5f27a21f607e6fcf67e34aadf3816f35bea923d'
+  sha256 '6c1c6fdf241100275ea988768610d7e60d1384c6c255345f4d4a08cdede44b4d'
+
+  url 'http://ridiculousfish.com/hexfiend/files/HexFiend.zip'
+  name 'HexFiend'
+  homepage 'http://ridiculousfish.com/hexfiend/'
+  license :bsd
+
+  app 'Hex Fiend.app'
+end
